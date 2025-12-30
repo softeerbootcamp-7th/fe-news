6ebb1562e4083bbdd9da05a0e3957919d6618e52
lwjmcn <clarecse02@gmail.com>
2025-12-30 17:26:08 +0900

    feat: news rolling animation with RAF

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 import getString from "@/test.js";
+import { NewArticlesView } from "@/newArticles.js";
+
+import { articlesData } from "/dummy/articlesData";
+
+const newArticlesView = new NewArticlesView(articlesData);
+newArticlesView.render();
 
 const list = document.getElementById("list");
 const newItem = document.createElement("li");
