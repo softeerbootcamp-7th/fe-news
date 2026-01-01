386fdfa5dbad97f52f663941b209c9336319203b
윤종근 <doodukly1999@gmail.com>
2026-01-01 23:07:47 +0900

    style(grid): 언론사 로고 영역 UI 구성

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ import '@/assets/css/style.css'
 import { getDate } from '@/js/Date.js';
 import { reload } from '@/js/Reload.js';
 import TitleLogo from '@/assets/images/light_mode_logo/title_logo.png';
+import GridView from '@/assets/images/light_mode_logo/grid-view.png';
+import ListView from '@/assets/images/light_mode_logo/list-view.png';
+import LeftButton from '@/assets/images/light_mode_logo/LeftButton.png';
+import RightButton from '@/assets/images/light_mode_logo/RightButton.png';
+
+
 
 document.querySelector('#app').innerHTML = `
   <div id="view">
@@ -24,8 +30,28 @@ document.querySelector('#app').innerHTML = `
         <div class="newsTitle">착한 소비 캠페인, 지역 상권 회복에 긍정적 영향</div>
       </li>
     </ul>
+    <div id="gridView">
+      <div id="tabAndViewer">
+        <div id="tabButton">
+          <div id="allPress">전체 언론사</div>
+          <div class="subscribedPress">
+            <div class="title">내가 구독한 언론사</div>
+            <div class="badge">8</div>
+          </div>
+        </div>
+        <div id="viewButton">
+          <img class="listViewIcon" src="${ListView}">
+          <img class="gridViewIcon" src="${GridView}">
+        </div>
+      </div>
+      <div id="viewArea">
+        <img class="left" src="${LeftButton}">
+        <div class="center"></div>
+        <img class="right" src="${RightButton}">
+      </div>
     </div>
   </div>
+
 `
 
 reload(document.querySelector('#title'));
