2442ec90eedb038edc54998780b6bff30d00dd27
bonsng <john.k7795@gmail.com>
2026-01-01 22:10:43 +0900

    feat: implement pagination and navigation for press grid

--- newsstand/src/components/createGrid.js
+++ newsstand/src/components/createGrid.js
@@ -1,10 +1,20 @@
 // src/components/pressGrid.js
 import { createEl } from "../lib/dom";
 import { getGridData } from "../lib/pressData";
+import { actions, store } from "../state/store";
 import { createGridCard } from "./createGridCard";
 
 export const createGrid = () => {
+  const state = store.getState();
   const gridList = getGridData();
+
+  // Pagination logic
+  const totalPages = Math.max(1, Math.min(4, Math.ceil(gridList.length / 24)));
+  const currentPage = Math.min(Math.max(state.page || 1, 1), totalPages);
+  const start = (currentPage - 1) * 24;
+  const end = start + 24;
+  const paginatedGridList = gridList.slice(start, end);
+
   const nsGrid = createEl(
     "section",
     "ns-press-grid border-default",
@@ -17,11 +27,53 @@ export const createGrid = () => {
 
   const list = nsGrid.querySelector(".ns-press-grid__list");
 
-  gridList.slice(0, 24).forEach((press) => {
+  // render paginated items
+  paginatedGridList.forEach((press) => {
     const item = createGridCard(press);
     list.appendChild(item);
   });
 
-  nsGrid.appendChild(list);
+  nsGrid.append(list, createGridNav(currentPage, totalPages));
+
+  // Navigation event listener
+  nsGrid.addEventListener("click", (e) => {
+    const navBtn = e.target.closest("button[data-nav]");
+    if (navBtn) {
+      const dir = navBtn.dataset.nav;
+      actions.setPage(
+        dir === "prev" ? currentPage - 1 : currentPage + 1,
+        totalPages
+      );
+    }
+  });
   return nsGrid;
+};
+
+const createGridNav = (page, totalPages) => {
+  const nav = createEl(
+    "nav",
+    "ns-grid-nav",
+
+    `${
+      page !== 1
+        ? `<button class="ns-grid-nav__btn grid__left" data-nav="prev" aria-label="이전 페이지">
+        <svg width="26" height="41" viewBox="0 0 26 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M24.781 0.384155L0.781006 20.3842L24.781 40.3842" stroke="#6E8091"/>
+        </svg>
+      </button>`
+        : ""
+    }
+     ${
+       page !== totalPages
+         ? `<button class="ns-grid-nav__btn grid__right" data-nav="next" aria-label="다음 페이지">
+        <svg width="26" height="41" viewBox="0 0 26 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0.320068 40.3842L24.3201 20.3842L0.320068 0.384155" stroke="#6E8091"/>
+        </svg>
+      </button>`
+         : ""
+     }`
+  );
+  nav.setAttribute("aria-label", "그리드뷰 네비게이션");
+
+  return nav;
 };
