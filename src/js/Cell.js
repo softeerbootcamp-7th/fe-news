63fd93b73569279ebd4b0f29382bdae950eee5db
윤종근 <doodukly1999@gmail.com>
2026-01-02 00:02:37 +0900

    design(cell): 실제 브랜드 로고 사용을 위한 셀 태그 수정

--- src/js/Cell.js
+++ src/js/Cell.js
@@ -1,13 +1,12 @@
 export function addCell() {
-    const newsBrandList = []
+    const newsBrandList = [];
 
+    
     for (let i = 1; i <= 24; i += 1) {
-        newsBrandList.push(`언론사${i}`);
+        newsBrandList.push(Math.floor(Math.random() * 93));
     }
 
-    const htmlString = newsBrandList.map(newsBrand => `<div class="grid-cell">
-        ${newsBrand}
-        </div>`).join('');
+    const htmlString = newsBrandList.map(newsBrand => `<img class="grid-cell" src="images/light_mode_logo/asset ${newsBrand} 1.png">`).join('');
 
     const targetElement = document.querySelector('#viewArea .center');
 
