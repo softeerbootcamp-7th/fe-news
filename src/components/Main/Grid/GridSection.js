a05a68fef592f91da0cb3cc42487911b3c4a702f
Stamp-Ho <inho8024s@gmail.com>
2026-01-04 14:22:45 +0900

    [FEAT] page 기능 구현

--- src/components/Main/Grid/GridSection.js
+++ src/components/Main/Grid/GridSection.js
@@ -3,31 +3,31 @@ import { GridSectionHeader } from "./GridSectionHeader";
 import { PressLogo } from "./PressLogo";
 import { Prev } from "../../icons/Previous";
 import { Next } from "../../icons/Next";
-import { pressList } from "./pressData";
+import { pageStore, pressStore } from "../../../store/index";
 import { Alert } from "../../Shared/Alert";
 
 export function GridSection() {
-  var isFirstPage = true;
-  var isLastPage = false;
+  const { currentPage, maxPage } = pageStore.state;
 
-  var page = 1;
-  var firstPress = (page - 1) * 24;
-  var lastPress = page * 24;
+  const { shuffledPressList } = pressStore.state;
+
   return `
     <section class="grid-section">
         ${GridSectionHeader(false, 8)}
         <div class="main-contents">
             ${Alert("우리언론", false)}
-            <button class="nav-button prev ${isFirstPage && "disabled"}">
+            <button class="nav-button prev ${currentPage === 0 && "disabled"}">
             ${Prev()}
             </button>
             <div class="press-logo-grid">
-              ${pressList
-                .slice(firstPress, lastPress)
+              ${shuffledPressList
+                .slice(currentPage * 24, (currentPage + 1) * 24)
                 .map((press) => PressLogo(press.logo))
                 .join("")}
             </div>
-            <button class="nav-button next ${isLastPage && "disabled"}">
+            <button class="nav-button next ${
+              currentPage === maxPage && "disabled"
+            }">
             ${Next()}
             </button>
         </div>
