db2f69e39882182106000cb59008eb2bc6a648fe
Stamp-Ho <inho8024s@gmail.com>
2026-01-04 14:19:36 +0900

    [FEAT] 로고 클릭시 새로고침 기능 + 이벤트 리스너 추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,10 @@
 import "./Header.css";
 import { Logo } from "../icons/Logo";
+import { todayString } from "../../utils/utils";
 
 export function Header() {
-  const today = new Date();
-
-  // 자동 포맷 설정
-  const dateString = new Intl.DateTimeFormat("ko-KR", {
-    dateStyle: "full", // "2025년 12월 30일 화요일" 형식 출력
-  })
-    .format(today)
-    .replace(/년 |월 /g, ". ") // "년", "월"을 "."으로 치환
-    .replace("일", "."); // "일"을 "."으로 치환
-
   return `<header class="titleHeader">
         <a class="title">${Logo()}뉴스스탠드</a>
-        <span class="data">${dateString}</span>
+        <span class="data">${todayString()}</span>
     </header>`;
 }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,58 @@
 import "./style.css";
-import "./styles/variables.css"; /* 1. 가장 먼저 변수를 불러옵니다 */
-import "./styles/reset.css"; /* 2. 브라우저 기본 스타일 초기화 */
+import "./styles/variables.css";
+import "./styles/reset.css";
 import { Header } from "./components/Header/Header.js";
 import { RollingSection } from "./components/Rollable/RollingSection.js";
 import { GridSection } from "./components/Main/Grid/GridSection.js";
+import { pageStore, viewStore, subsStore } from "./store/index.js"; // 스토어 가져오기
 
-document.querySelector("#app").innerHTML = `
+const app = document.querySelector("#app");
+
+// 1. 렌더링 함수 정의
+function render() {
+  app.innerHTML = `
       ${Header()}
       <main>
         ${RollingSection()}
         ${GridSection()}
       </main>
     `;
+}
+
+// 2. 초기 실행
+render();
+
+// 3. 상태 변화 구독 (스토어가 변하면 다시 렌더링)
+window.addEventListener("pageStoreChange", render);
+window.addEventListener("viewStoreChange", render);
+window.addEventListener("subsStoreChange", render);
+
+app.addEventListener("click", (e) => {
+  const target = e.target;
+
+  /** 헤더 로고 클릭 → 새로고침
+   * 새로고침시 css불러오기까지 지연 발생. 안이쁨 수정 고민해봐야함
+   **/
+  if (target.closest(".titleHeader .title")) {
+    e.preventDefault();
+    location.reload();
+    return;
+  }
+
+  // 다음 페이지 버튼 클릭
+  if (target.closest(".nav-button.next")) {
+    pageStore.setPage(pageStore.state.currentPage + 1);
+  }
+
+  // 이전 페이지 버튼 클릭
+  if (target.closest(".nav-button.prev")) {
+    pageStore.setPage(pageStore.state.currentPage - 1);
+  }
+
+  // 구독 버튼 클릭
+  const subBtn = target.closest(".subscribe-button");
+  if (subBtn) {
+    const pressId = subBtn.dataset.id;
+    subsStore.toggleSub(pressId);
+  }
+});
