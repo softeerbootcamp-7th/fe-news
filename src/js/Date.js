d1bf1c02b2d15b9183921ba4a50125382c455f03
윤종근 <doodukly1999@gmail.com>
2026-01-04 22:38:24 +0900

    fix(date): getDay()의 0이 일요일이 되도록 days 리스트 순서 변경

--- src/js/Date.js
+++ src/js/Date.js
@@ -4,7 +4,7 @@ export function getDate() {
   const month = date.getMonth() + 1;
   const day = date.getDate();
   const dayOfWeek = date.getDay();
-  const days = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'];
-  
-  return `${year}. ${month}. ${day}. ${days[dayOfWeek - 1]}`;
+  const days = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+
+  return `${year}. ${month}. ${day}. ${days[dayOfWeek]}`;
 }
