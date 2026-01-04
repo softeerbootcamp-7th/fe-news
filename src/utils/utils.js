2f4308f61b7db7028c0d4614b9087bc59d762a65
Stamp-Ho <inho8024s@gmail.com>
2026-01-04 14:22:16 +0900

    [FIX] uitls.js 추가

---
+++ src/utils/utils.js
@@ -0,0 +1,22 @@
+export function shuffle(array) {
+  const tempArr = [...array];
+  for (let i = tempArr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [tempArr[i], tempArr[j]] = [tempArr[j], tempArr[i]];
+  }
+  return tempArr;
+}
+
+export function todayString() {
+  const today = new Date();
+
+  // 자동 포맷 설정
+  const dateString = new Intl.DateTimeFormat("ko-KR", {
+    dateStyle: "full", // "2025년 12월 30일 화요일" 형식 출력
+  })
+    .format(today)
+    .replace(/년 |월 /g, ". ") // "년", "월"을 "."으로 치환
+    .replace("일", "."); // "일"을 "."으로 치환
+
+  return dateString;
+}
