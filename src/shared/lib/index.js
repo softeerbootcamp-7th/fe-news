238c6376aa7418e101216496bb079e7d8011c5aa
rkdwoals159 <rkdwoals159@gmail.com>
2026-01-05 18:15:17 +0900

    chore: trim shared lib barrel
    - drop widget-specific export from shared barrel

--- src/shared/lib/index.js
+++ src/shared/lib/index.js
@@ -1,7 +1,6 @@
 export * from "./array.js";
 export * from "./date.js";
 export * from "./dom.js";
-export * from "../../widgets/newsRolling/lib/extractHeadlinesFromJson.js";
 export * from "./newsLogos.js";
 export * from "./pressNameFromFilename.js";
 export * from "./theme.js";
