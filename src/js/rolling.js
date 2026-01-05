505777bc89e775bdb5b8e041b8c266f0358aca6a
윤종근 <doodukly1999@gmail.com>
2026-01-05 17:28:26 +0900

    feat(rolling): 뉴스 헤드라인에 마우스 hover 시 롤링 정지 기능 추가

--- src/js/rolling.js
+++ src/js/rolling.js
@@ -8,20 +8,70 @@ function setHeadLine(element, press, title, wrapper) {
     wrapper.value += 1;
 }
 
-function repeatRolling(element, press, title, index) {
-    setHeadLine(element, press, title, index)
-    setTimeout(repeatRolling, 5000, element, press, title, index);
+function repeatRolling(element, press, title, index, state, timer) {
+    if (state.value === false) {
+        setHeadLine(element, press, title, index)
+        timer.value = setTimeout(repeatRolling, 5000, element, press, title, index, state, timer);
+    } else {
+        clearTimeout(timer.value);
+    }
 }
 
 export function rollingHeadLine() {
+    function initHeadLine() {
+        setHeadLine(leftTargetElement, pressHtmlString, titleHtmlString, indexWrapper);
+        setHeadLine(rightTargetElement, pressHtmlString, titleHtmlString, indexWrapper);
+    }
+
+    function setHoverEvent(element, state) {
+        element.addEventListener('mouseenter', () => {
+            state.value = true;
+        });
+
+        element.addEventListener('mouseleave', () => {
+            state.value = false;
+            if (leftTimer.value !== null) {
+                clearTimeout(leftTimer.value);
+                leftTimer.value = null;
+            }
+
+            if (rightTimer.value !== null) {
+                clearTimeout(rightTimer.value);
+                rightTimer.value = null;
+            }
+
+            setTimeout(() => {
+                repeatRolling(leftTargetElement, pressHtmlString, titleHtmlString, indexWrapper, hoverState, rightTimer);
+            }, 5000);
+
+            setTimeout(() => {
+                repeatRolling(rightTargetElement, pressHtmlString, titleHtmlString, indexWrapper, hoverState, rightTimer);
+            }, 6000);
+            
+        });
+    }
+
+    const bothTargetElement = document.querySelector('#autoRollingNewsBar');
     const leftTargetElement = document.querySelector('#autoRollingNewsBar .left');
     const rightTargetElement = document.querySelector('#autoRollingNewsBar .right');
     const pressHtmlString = data.map(each => `<div class="press">${each.press}</div>`);
     const titleHtmlString = data.map(each => `<div class="newsTitle">${each.mainTitle}</div>`);
     let indexWrapper = {
-        value: 1
+        value: 0
+    };
+    const hoverState = {
+        value: false
     };
 
+    const leftTimer = {
+        value: null
+    }
+
+    const rightTimer = {
+        value: null
+    }
+    
+    setHoverEvent(bothTargetElement, hoverState);
     initHeadLine();
     setTimeout(() => {
         repeatRolling(leftTargetElement, pressHtmlString, titleHtmlString, indexWrapper, hoverState, rightTimer);
