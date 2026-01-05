dcf74db7d1cf913143cc7883bf3910d5b7ac91dd
rkdwoals159 <rkdwoals159@gmail.com>
2026-01-05 17:34:32 +0900

    fix: consolidate shared utils
    - move pressNameFromFilename into shared lib
    
    - update shared barrel exports
    
    - remove unused widget lib duplicate

--- src/shared/lib/index.js
+++ src/shared/lib/index.js
@@ -3,5 +3,5 @@ export * from "./date.js";
 export * from "./dom.js";
 export * from "../../widgets/newsRolling/lib/extractHeadlinesFromJson.js";
 export * from "./newsLogos.js";
-export * from "../../features/subscriptions/lib/pressNameFromFilename.js";
+export * from "./pressNameFromFilename.js";
 export * from "./theme.js";
