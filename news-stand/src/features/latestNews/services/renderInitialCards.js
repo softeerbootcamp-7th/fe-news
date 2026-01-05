4aba31e1b86f05acf49e60382d6055e30d4a996c
arty0928 <arty0928@konkuk.ac.kr>
2026-01-05 18:02:24 +0900

    fix: dom 노드 생성이 아닌 이미 있는 html 내용 업데이트로 FOUC 문제 해결

--- news-stand/src/features/latestNews/services/renderInitialCards.js
+++ news-stand/src/features/latestNews/services/renderInitialCards.js
@@ -1,23 +1,9 @@
-import { createLatestNewsCard } from '../components/LatestNewsCard.js';
+import { updateLatestNewsCard } from '../components/LatestNewsCard.js';
 
 /**
  * 초기 카드 렌더링
  */
 export function renderInitialCards(initialNews) {
-  const container = document.getElementById('news-container');
-
-  if (!container) {
-    console.error('news-container를 찾을 수 없습니다');
-    return;
-  }
-
-  const cardsWrapper = document.createElement('div');
-  cardsWrapper.className = 'cards-wrapper';
-
-  const card1 = createLatestNewsCard(1, initialNews[0]);
-  const card2 = createLatestNewsCard(2, initialNews[1]);
-
-  cardsWrapper.appendChild(card1);
-  cardsWrapper.appendChild(card2);
-  container.appendChild(cardsWrapper);
+  updateLatestNewsCard(1, initialNews[0]);
+  updateLatestNewsCard(2, initialNews[1]);
 }
