e04174b02b9ccb467071eeeb250e7789a0c2c91c
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-05 16:40:41 +0900

    refactor: entry file 위치 변경

--- src/components/main.js
+++
@@ -1,8 +0,0 @@
-import { TodayDate } from './today-date';
-import { RollingSection } from './rolling-section';
-export const main = async () => {
-  TodayDate();
-  await RollingSection();
-};
-
-main();
