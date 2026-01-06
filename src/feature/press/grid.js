a74e8aa32dce7cff1916ae36ee545ed37e080be5
lwjmcn <clarecse02@gmail.com>
2026-01-06 15:49:02 +0900

    feat: 페이지네이션 기능 및 버튼 strategy 패턴으로 분리

--- src/feature/press/grid.js
+++ src/feature/press/grid.js
@@ -1,4 +1,3 @@
-import { articlesData } from "@/dummy/articlesData";
 import {
   getPressGridContainerTemplate,
   getPressGridItemTemplate,
@@ -6,8 +5,6 @@ import {
   getSubscribeButtonTemplate,
   getCancelButtonTemplate,
 } from "@/template/GridView";
-import { shuffleArray } from "@/utils/shuffle";
-import { parsePressData } from "@/utils/parse";
 import {
   isSubscribed,
   toggleSubscription,
@@ -15,79 +12,18 @@ import {
 } from "@/store/subscription";
 import { getLoadingIndicatorTemplate } from "@/template/Loading";
 
-const NUM_GRID_ROW = 4;
-const NUM_GRID_COL = 6;
-const NUM_GRID_CELL = NUM_GRID_COL * NUM_GRID_ROW;
-
-const gridContainer = document.querySelector(".press-section");
-const prevButton = document.querySelector(".press-list__control--prev");
-const nextButton = document.querySelector(".press-list__control--next");
-
-let currentPage = 0;
-let pressData = [];
-
-let isBound = false;
-
-export function initGridView(parsedPressData) {
-  // 데이터 셔플
-  pressData = shuffleArray(parsedPressData);
-
+export function initGridView(paginatedPressData) {
   // 첫 grid 레이아웃 그리기
   renderGridContainer();
-  renderCurrentPage();
+  renderGridItems(paginatedPressData);
 
-  // 버튼 이벤트 등록
-  if (!isBound) {
-    addEvents();
-    isBound = true;
-  }
+  // 구독하기 버튼 이벤트 등록
+  addClickEvents();
 
   // 구독 상태 observer 등록
   observeSubscriptionStore((pressId) => {
     updateGridItem(pressId);
   });
-}
-
-function addEvents() {
-  // 구독하기 버튼
-  gridContainer.addEventListener("click", (e) => {
-    const pressId = e.target.closest("li").dataset.label;
-    if (!pressId) return;
-    toggleSubscription(pressId);
-    e.target.closest("button").innerHTML = getLoadingIndicatorTemplate();
-  });
-  // 양옆 화살표 버튼
-  prevButton.addEventListener("click", () => {
-    currentPage--;
-    renderCurrentPage();
-  });
-  nextButton.addEventListener("click", () => {
-    currentPage++;
-    renderCurrentPage();
-  });
-}
-
-function getPageCount(pressData) {
-  return Math.ceil(pressData.length / NUM_GRID_CELL) || 1;
-}
-
-function getPaginationData(pageNum, pressData) {
-  return pressData.slice(
-    NUM_GRID_CELL * pageNum,
-    NUM_GRID_CELL * (pageNum + 1)
-  );
-}
-
-function updateArrowVisibility() {
-  const pageCount = getPageCount(pressData);
-  prevButton.classList.toggle("hidden", currentPage === 0);
-  nextButton.classList.toggle("hidden", currentPage === pageCount - 1);
-}
-
-function renderCurrentPage() {
-  const paginatedData = getPaginationData(currentPage, pressData);
-  renderGridItems(paginatedData);
-  updateArrowVisibility();
 }
 
 function renderGridItems(paginatedData) {
@@ -98,24 +34,36 @@ function renderGridItems(paginatedData) {
 
   paginatedData.forEach((item) => {
     // grid item template
-    gridContentHTML += getPressGridItemTemplate({
-      pressId: item.id,
-      pressName: item.name,
-      logoSrc: item.logo.replace("light", currentTheme),
-      isSubscribed: isSubscribed(item.id),
-    });
+    if (item)
+      gridContentHTML += getPressGridItemTemplate({
+        pressId: item.id,
+        pressName: item.name,
+        logoSrc: item.logo.replace("light", currentTheme),
+        isSubscribed: isSubscribed(item.id),
+      });
+    // empty cell
+    else gridContentHTML += getEmptyGridItemTemplate();
   });
 
-  // 남은 셀
-  if (paginatedData.length < NUM_GRID_CELL)
-    for (let i = paginatedData.length; i < NUM_GRID_CELL; i++)
-      gridContentHTML += getEmptyGridItemTemplate();
+  document.querySelector(".press-grid__list").innerHTML = gridContentHTML;
+}
 
-  gridContainer.firstElementChild.innerHTML = gridContentHTML;
+function addClickEvents() {
+  // 구독하기 버튼
+  const gridContainer = document.querySelector(".press-grid__list");
+  gridContainer.addEventListener("click", (e) => {
+    console.log("click");
+    const pressId = e.target.closest("li").dataset.label;
+    if (!pressId) return;
+    const button = e.target.closest("button");
+    if (!button) return;
+    button.innerHTML = getLoadingIndicatorTemplate();
+    toggleSubscription(pressId);
+  });
 }
 
 function updateGridItem(pressId) {
-  const li = gridContainer.querySelector(`li[data-label="${pressId}"]`);
+  const li = document.querySelector(`li[data-label="${pressId}"]`);
   if (!li) return;
   const button = li.querySelector("button");
   // 버튼 상태만 갱신
@@ -125,5 +73,6 @@ function updateGridItem(pressId) {
 }
 
 function renderGridContainer() {
-  gridContainer.innerHTML = getPressGridContainerTemplate();
+  document.querySelector(".press-section").innerHTML =
+    getPressGridContainerTemplate();
 }
--- src/feature/press/press.js
+++ src/feature/press/press.js
@@ -1,6 +1,5 @@
 import { parsePressData } from "@/utils/parse";
-import { initGridView } from "./grid";
-import { initListView } from "./list";
+import { shuffleArray } from "@/utils/shuffle";
 import { SUBSCRIPTION_TAB } from "@/types/constant";
 import { VIEW_TAB } from "@/types/constant";
 import {
@@ -14,11 +13,82 @@ import {
   setSubscriptionTab,
 } from "@/store/subscriptionTab";
 import { getViewTab, observeViewTabStore, setViewTab } from "@/store/viewTab";
+import { createPaginationController } from "./pagination";
+import { initGridView } from "./grid";
+import { initListView } from "./list";
+
+// 상태
+let filteredData = [];
+const pagination = createPaginationController();
 
 export function initPressView(articlesData) {
-  let parsedPressData = parsePressData(articlesData); // {id, name, logo}
+  // dummy 데이터 파싱 및 셔플
+  const allPressData = shuffleArray(parsePressData(articlesData)); // {id, name, logo}
 
-  // 전체/구독 언론사 조정
+  // 그리드/리스트 버튼
+  addViewTabEvents();
+  // 전체/구독 버튼
+  addSubscriptionTabEvents();
+  // 페이지네이션 버튼
+  addPaginationEvents();
+
+  // 그리드/리스트 변경 시 뷰 업데이트
+  observeViewTabStore(() => {
+    pagination.setStrategy(getViewTab());
+    renderPressView();
+  });
+  // 전체/구독 탭 변경 시 데이터 업데이트
+  observeSubscriptionTabStore(() => {
+    filteredData = getFilteredPressData(allPressData);
+    pagination.reset();
+    renderPressView();
+  });
+
+  // 구독/해지 변경 시 뷰 업데이트
+  observeSubscriptionStore(() => updateSubscriptionCount());
+
+  // 초기 설정
+  setViewTab(VIEW_TAB.GRID);
+  setSubscriptionTab(SUBSCRIPTION_TAB.ALL);
+  updateSubscriptionCount();
+}
+
+function getFilteredPressData(allPressData) {
+  let filteredData = allPressData;
+  if (getSubscriptionTab() === SUBSCRIPTION_TAB.MY)
+    filteredData = allPressData.filter((press) => isSubscribed(press.id));
+  return filteredData;
+}
+
+function renderPressView() {
+  // 그리드/리스트 뷰
+  const paginatedData = pagination.getPageData(filteredData);
+  const { showPrev, showNext } = pagination.getArrowState(filteredData);
+
+  switch (getViewTab()) {
+    case VIEW_TAB.GRID:
+      initGridView(paginatedData);
+      break;
+    case VIEW_TAB.LIST:
+      initListView();
+      break;
+  }
+
+  // 좌우 페이지네이션 화살표
+  const prevButton = document.querySelector(".press-list__control--prev");
+  const nextButton = document.querySelector(".press-list__control--next");
+  prevButton.classList.toggle("hidden", !showPrev);
+  nextButton.classList.toggle("hidden", !showNext);
+}
+
+function updateSubscriptionCount() {
+  const subscriptionCount = getSubscriptionCount();
+  const [_, myViewButton] = document.querySelectorAll(".press-news__tab");
+  myViewButton.querySelector("span").textContent = subscriptionCount;
+}
+
+function addSubscriptionTabEvents() {
+  // 전체 언론사 뷰 / 내가 구독한 언론사 뷰 버튼
   const [allViewButton, myViewButton] =
     document.querySelectorAll(".press-news__tab");
   allViewButton.addEventListener("click", () => {
@@ -31,11 +101,9 @@ export function initPressView(articlesData) {
     allViewButton.classList.remove("active");
     setSubscriptionTab(SUBSCRIPTION_TAB.MY);
   });
-  observeSubscriptionTabStore(() => {
-    updateSubscriptionTab(parsedPressData);
-  });
-
-  // 그리드/리스트 뷰 조정
+}
+function addViewTabEvents() {
+  // 그리드/리스트 뷰 버튼
   const [listViewButton, gridViewButton] =
     document.querySelectorAll(".view-toggle");
 
@@ -49,37 +117,19 @@ export function initPressView(articlesData) {
     gridViewButton.classList.remove("active");
     setViewTab(VIEW_TAB.LIST);
   });
+}
+function addPaginationEvents() {
+  // 페이지네이션 양옆 화살표
+  const prevButton = document.querySelector(".press-list__control--prev");
+  const nextButton = document.querySelector(".press-list__control--next");
 
-  // 그리드/리스트 뷰에 따라 업데이트
-  updateViewTab(parsedPressData);
-  observeViewTabStore(() => {
-    updateViewTab(parsedPressData);
+  prevButton.addEventListener("click", () => {
+    pagination.prev();
+    renderPressView();
   });
 
-  updateSubscriptionCount(myViewButton);
-  observeSubscriptionStore(() => {
-    updateSubscriptionCount(myViewButton);
+  nextButton.addEventListener("click", () => {
+    pagination.next();
+    renderPressView();
   });
-}
-
-function updateSubscriptionCount(myViewButton) {
-  myViewButton.querySelector("span").textContent = getSubscriptionCount();
-}
-
-function updateSubscriptionTab(pressData) {
-  let filteredData = pressData;
-  if (getSubscriptionTab() === SUBSCRIPTION_TAB.MY)
-    filteredData = pressData.filter((press) => isSubscribed(press.id));
-  updateViewTab(filteredData);
-}
-function updateViewTab(pressData) {
-  const viewTab = getViewTab();
-  switch (viewTab) {
-    case VIEW_TAB.GRID:
-      initGridView(pressData);
-      break;
-    case VIEW_TAB.LIST:
-      initListView();
-      break;
-  }
 }
