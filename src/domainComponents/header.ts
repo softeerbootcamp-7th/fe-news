36412f027bec38897c0478384d37764946dec58d
pakxe <pigkill40@naver.com>
2026-01-06 14:05:46 +0900

    feat: Header를 렌더링하는 컴포넌트 구현

---
+++ src/domainComponents/header.ts
@@ -0,0 +1,13 @@
+import { renderLogo } from "../components/logo";
+import { renderCurDate } from "../components/curDate";
+
+export function renderHeader() {
+	const headerDOM = document.createElement("header");
+	headerDOM.className = "flex flex-row justify-between";
+
+	const LogoDOM = renderLogo();
+
+	headerDOM.append(LogoDOM, renderCurDate());
+
+	return headerDOM;
+}
