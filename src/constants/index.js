b64ab93c017cdfe6060300cd6ce950fe077f053f
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-06 16:01:53 +0900

    feat: 구독 버튼 관련 템플릿 및 css 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,11 @@
 export { DAY_OF_WEEK } from './day-of-week.js';
 export { NUMBER_OF_NEWSPAPER } from './number-of-newspaper.js';
-export { rollingItemTemplate } from './rolling-item-template.js';
 export { ROLLING_ANIMATION } from './rolling-animation.js';
+export { GRID_VIEW } from './grid-view.js';
+export { LEFT_ARROW, RIGHT_ARROW, PLUS } from './svgs';
+export {
+  ROLLING_ITEM_TEMPLATE,
+  GRID_ITEM_TEMPLATE,
+  SUBSCRIBE_BUTTON_TEMPLATE,
+  LOGO_IMAGE_TEMPLATE,
+} from './templates';
---
+++ src/constants/templates/subscribe-button-template.js
@@ -0,0 +1,9 @@
+import { PLUS } from '../svgs';
+
+export const SUBSCRIBE_BUTTON_TEMPLATE = () => {
+  return `
+  <button class="subscribe-button">
+    ${PLUS}
+    구독하기
+  </button>`;
+};
