9cc6fe56029a6f87bfcd68b3dc165d4c0125d6d9
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-06 23:53:07 +0900

    feat: x icon svg 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,4 +2,4 @@ export { DAY_OF_WEEK } from './day-of-week.js';
 export { NUMBER_OF_NEWSPAPER } from './number-of-newspaper.js';
 export { ROLLING_ANIMATION } from './rolling-animation.js';
 export { GRID_VIEW } from './grid-view.js';
-export { LEFT_ARROW, RIGHT_ARROW, PLUS } from './svgs';
+export { LEFT_ARROW, RIGHT_ARROW, PLUS, X } from './svgs';
--- src/constants/svgs/index.js
+++ src/constants/svgs/index.js
@@ -1,3 +1,4 @@
 export { LEFT_ARROW } from './left-arrow.js';
 export { RIGHT_ARROW } from './right-arrow.js';
 export { PLUS } from './plus.js';
+export { X } from './x.js';
