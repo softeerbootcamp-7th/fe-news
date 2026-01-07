4f2c65c3e1b6dbf8638d84de92a5e616afa3b899
KwonYongHyun <a48209952@gmail.com>
2026-01-07 16:33:13 +0900

    feat: add hover overlay to grid items

--- grid/index.js
+++ grid/index.js
@@ -2,6 +2,34 @@ import { GRID_COLS, GRID_ROWS } from "./config.js";
 import logoLinks from '../data/logo_links.json';
 import { fisherYatesShuffle } from '../utils';
 
+// 오버레이 너비, 높이, 위치 계산하는 함수
+function updateOverlayPosition(overlay, itemRect, gridRect) {
+    const nextWidth = itemRect.width;
+    const nextHeight = itemRect.height;
+
+    if(overlay.offsetWidth !== nextWidth) overlay.style.width = `${nextWidth}px`;
+    if(overlay.offsetHeight !== nextHeight) overlay.style.height = `${nextHeight}px`;
+    overlay.style.transform = `translate(${itemRect.left - gridRect.left}px, ${itemRect.top - gridRect.top}px)`;
+}
+
+// 오버레이 보여주는 함수
+function showGridOverlay(event, gridElement, overlay) {
+    const gridItem = event.target.closest('.content__grid-item');
+    if (!gridItem) return;
+    
+    const itemRect = gridItem.getBoundingClientRect();
+    const gridRect = gridElement.getBoundingClientRect();
+
+    updatePosition(overlay, itemRect, gridRect);
+
+    overlay.classList.remove('grid__overlay--hidden');
+}
+
+// 오버레이 숨기는 함수
+function hiddenGridOverlay(overlay) {
+    overlay.classList.add('grid__overlay--hidden');
+}
+
 // 그리드 초기화 함수
 function initGrid({ containerSelector }) {
     const gridElement = document.querySelector(containerSelector);
@@ -25,6 +53,11 @@ function initGrid({ containerSelector }) {
         
         gridElement.appendChild(item);
     })
+
+    const overlay = document.querySelector('.grid__overlay');
+
+    gridElement.addEventListener('mouseover', (event) => showGridOverlay(event, gridElement, overlay));
+    gridElement.addEventListener('mouseleave', () => hiddenGridOverlay(overlay));
 }
 
 export { initGrid };
