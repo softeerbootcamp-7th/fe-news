0a789635bfafb59092b303b97d0c0064c000af6f
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-07 17:29:42 +0900

    feat: 전체 언론사 전용 이벤트 핸들러 추가

---
+++ src/models/handlers/total-grid-view-event-handler.js
@@ -0,0 +1,40 @@
+import { subscribedNewspaperStore } from '@/stores';
+import {
+  unsubscribeButtonTemplate,
+  subscribeButtonTemplate,
+} from '@/templates';
+
+/**
+ * @typedef {import('../../types').Newspaper} Newspaper
+ *
+ * @typedef {Object} TotalGridViewEventHandlerParams
+ * @property {Newspaper[]} newspaperList
+ *
+ * @param {TotalGridViewEventHandlerParams} totalGridViewEventHandlerParams
+ * @returns {{handleClick: (event: Event) => void}}
+ */
+export const totalGridViewEventHandler = ({ newspaperList }) => {
+  const handleClick = (event) => {
+    const $card = event.target.closest('.news-grid-view__card');
+    const newspaperIndex = $card?.getAttribute('data-index');
+
+    if (!$card || !newspaperIndex) {
+      return;
+    }
+    if (event.target.closest('.subscribe-button')) {
+      subscribedNewspaperStore.subscribeNewspaper(
+        newspaperList[newspaperIndex],
+      );
+      $card.innerHTML = unsubscribeButtonTemplate();
+    } else if (event.target.closest('.unsubscribe-button')) {
+      subscribedNewspaperStore.unsubscribeNewspaper(
+        newspaperList[newspaperIndex].press,
+      );
+      $card.innerHTML = subscribeButtonTemplate();
+    }
+  };
+
+  return {
+    handleClick,
+  };
+};
