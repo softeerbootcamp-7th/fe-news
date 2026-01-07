5f6bf9581effebb12bd6dec22c6ed14a6d38ceca
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-07 22:58:23 +0900

    feat: view mode에 따른 grid view 조건부 렌더링 로직 추가

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,11 +1,49 @@
+import { NEWS_SECTION_STATE } from '@/constants';
+import { Observer } from '@/libs';
+import { getNewspaperForGrid } from '@/models';
+import { newsSectionStore } from '@/stores';
+
 import { GridView } from '../grid-view';
 import { NewsSectionHeader } from '../news-section-header';
 import { RollingSection } from '../rolling-section';
 import { SubscribedNewsNumber } from '../subscribed-news-number';
 
-export const Main = () => {
+export const Main = async () => {
+  let currentView = newsSectionStore.getState().view;
+  let gridViewCleanupFunctions = null;
+  const { newspaperList: totalNewspaperList } = await getNewspaperForGrid();
+
+  const updateMain = () => {
+    const newView = newsSectionStore.getState().view;
+    if (currentView === newView) {
+      return;
+    }
+
+    currentView = newView;
+
+    if (newView === NEWS_SECTION_STATE.VIEW.LIST) {
+      if (gridViewCleanupFunctions) {
+        gridViewCleanupFunctions();
+      }
+      return;
+    }
+
+    GridView({ newspaperList: totalNewspaperList }).then(
+      ({ cleanup: cleanupGridView }) => {
+        gridViewCleanupFunctions = cleanupGridView;
+      },
+    );
+  };
+
+  const observer = new Observer(updateMain);
+  newsSectionStore.subscribe(observer);
+
   RollingSection();
   NewsSectionHeader();
   SubscribedNewsNumber();
-  GridView();
+  GridView({ newspaperList: totalNewspaperList }).then(
+    ({ cleanup: cleanupGridView }) => {
+      gridViewCleanupFunctions = cleanupGridView;
+    },
+  );
 };
