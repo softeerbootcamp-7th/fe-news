75def4fdc77a56f0ec76926808ff563e62ae483e
HIHJH <2170095@ewhain.net>
2026-01-09 07:59:20 +0900

    feat: 상태 관리를 위한 스토어 정리 중 - 옵저버 패턴

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,23 @@
 // 그리드 뷰) 한 페이지 당 언론사 개수
-export const ITEMS_PER_PAGE = 24
+export const ITEMS_PER_PAGE = 24;
+
+export const VIEW_MODE = {
+  GRID: "grid",
+  LIST: "list",
+};
+
+export const TAB = {
+  ALL: "ALL",
+  SUBSCRIBED: "SUBSCRIBED",
+};
+
+export const ACTION = {
+  SET_VIEW: "SET_VIEW",
+  SET_TAB: "SET_TAB",
+  SET_PAGE: "SET_PAGE",
+
+  FETCH_PRESSES: "FETCH_PRESSES",
+
+  SET_SUBSCRIPTIONS: "SET_SUBSCRIPTIONS",
+  TOGGLE_SUBSCRIPTION: "TOGGLE_SUBSCRIPTION",
+};
