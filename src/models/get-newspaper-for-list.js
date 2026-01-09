dd10f8689b51dfcafaaa26121ff976393c54a37b
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-09 13:49:26 +0900

    feat: 전체 뉴스 기사를 받아와서 카테고리 별로 분류된 map을 반환하도록 변경

--- src/models/get-newspaper-for-list.js
+++ src/models/get-newspaper-for-list.js
@@ -1,16 +1,26 @@
 import { fetchNewspaper } from '@/apis';
+import { shuffleArray } from '@/utils';
 
 /**
  *
  * @typedef {import('../types').Newspaper} Newspaper
- * @param {Object} params
- * @param {string} params.category
- * @returns {Promise<Newspaper[]>}
+ * @returns {Promise<{ categoryNewspaperMap: Map<Newspaper['category'], Newspaper[]> }>}
  */
-export const getNewspaperForList = async ({ category }) => {
-  const newspaperList = await fetchNewspaper(`category=${category}`);
+export const getNewspaperForList = async () => {
+  const newspaperList = await fetchNewspaper();
+
+  const shuffledNewspaperList = shuffleArray(newspaperList);
+
+  const categoryNewspaperMap = new Map();
+  shuffledNewspaperList.forEach((newspaper) => {
+    const category = newspaper.category;
+    if (!categoryNewspaperMap.has(category)) {
+      categoryNewspaperMap.set(category, []);
+    }
+    categoryNewspaperMap.get(category).push(newspaper);
+  });
 
   return {
-    newspaperList,
+    categoryNewspaperMap,
   };
 };
