73f3611153460a03e36f9083d17a049d8ca035b6
lwjmcn <clarecse02@gmail.com>
2026-01-09 11:44:41 +0900

    feat: 더미데이터 fetch

--- src/app.js
+++ src/app.js
@@ -3,16 +3,37 @@ import { LatestNewsView } from "@/feature/latest-news/latestNews.js";
 import { initPressView } from "@/feature/press/press";
 import { initTheme } from "@/feature/header/theme";
 
-// 다크모드 테마
-initTheme();
+initApp();
 
-// 기본 상단 영역
-initHeader();
+function initApp() {
+  // 다크모드 테마
+  initTheme();
 
-// 뉴스 자동 롤링
-const latestNewsView = new LatestNewsView();
-latestNewsView.initLatestNews(articlesData);
+  // 기본 상단 영역
+  initHeader();
 
-initPressView(articlesData);
+  // 뉴스 콘텐츠 영역
+  initNews();
+}
 
-// 리스트 영역
+function initNews() {
+  // 데이터 fetch
+  fetch("./src/assets/dummy/articlesData.json")
+    .then((response) => response.json())
+    .then((articlesData) => {
+      // 뉴스 자동 롤링
+      const latestNewsView = new LatestNewsView();
+      latestNewsView.initLatestNews(articlesData);
+
+      // 그리드/리스트 영역
+      initPressView(articlesData);
+
+      return articlesData;
+    })
+    .catch((err) => {
+      console.log(err);
+      document.querySelector(
+        ".newsstand-main"
+      ).innerHTML = `<span>데이터를 불러오는 중 오류가 발생했습니다.</span>`;
+    });
+}
--- src/feature/header/header.js
+++ src/feature/header/header.js
@@ -1,11 +1,13 @@
 import { formatDate } from "@/utils/parse";
+import { setViewTab, getViewTab } from "@/store/viewTabStore";
+import { VIEW_TAB } from "@/types/constant";
 
 export function initHeader() {
   // 로고 클릭 시 새로고침
   document
-    .querySelector(".newsstand-header__logo")
+    .querySelector(".newsstand-header__left")
     .addEventListener("click", () => {
-      location.reload();
+      setViewTab(getViewTab()); // 리렌더링 트리거
     });
 
   // 날짜 설정
