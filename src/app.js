6c200dea756e8a8ea1fb372d2bd43f7311c4e767
lwjmcn <clarecse02@gmail.com>
2026-01-09 11:44:03 +0900

    chore: 더미데이터 json 파일로 교체

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-import { articlesData } from "@/assets/dummy/articlesData";
 import { initHeader } from "@/feature/header/header";
 import { LatestNewsView } from "@/feature/latest-news/latestNews.js";
 import { initPressView } from "@/feature/press/press";
