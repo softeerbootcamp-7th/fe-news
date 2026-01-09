ef2f47ad384ac4697c355a31e3dc1331a3f87a7b
HIHJH <2170095@ewhain.net>
2026-01-09 12:15:56 +0900

    feat: data fetch 하여 데이터 상태 반영 및 로컬 스토리지에서 구독 정보 받아서 구독 상태 반영 - app.js에 두었으나 로직 정리가 필요함

--- src/app.js
+++ src/app.js
@@ -2,11 +2,20 @@ import './style.css'
 import { displayTodayDate } from './utils/date.js'
 import { initNewsView } from './features/news-contents/newsViewController.js'
 import { rolling } from './features/rolling-news/rollingController.js'
-import { newsState } from './stores/newsState.js'
+import { store } from './stores/store.js'
+import { ACTION } from './constants/constants.js'
+import { fetchPressData } from './services/fetchPressData.js'
+import { setupSubscriptionSync } from './stores/effects.js'
 
+// TODO: 로직 정리 필요
 async function init() {
   displayTodayDate() // 헤더에 오늘 날짜 표시
-  await newsState.init() // 뉴스 상태 데이터 로드
+
+  const presses = await fetchPressData()
+  store.dispatch({ type: ACTION.FETCH_PRESSES, payload: presses }) // 언론사 데이터 로드
+
+  setupSubscriptionSync(store) // 구독 정보 로드 및 동기화 설정
+
   rolling.init() // 롤링 뉴스 초기화
   await initNewsView() // 뉴스 콘텐츠 뷰 컨트롤러 초기화
 }
